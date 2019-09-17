2a72c09483fbc47047e85d8fba96d4e569ba69e4
William Blacknall <williamblacknall@gmail.com>
2019-09-17 14:49:21 -0400

    Added final exercise

--- jsexercises.js
+++ jsexercises.js
@@ -14,6 +14,21 @@ console.log(madLib("William", "Science"))
 //uses function to determine tip amount
 const tipAmount = (billAmount, serviceLevel) => {
     if (serviceLevel == "fair"){
+        return billAmount * .15;
+    }   
+    if (serviceLevel == "good"){
+        return billAmount * .2
+    }
+    if (serviceLevel == "bad"){
+        return billAmount * .1
+    }
+}
+
+console.log(tipAmount(100, "good"))
+
+//same as above but returns total bill amount
+const totalAmount = (billAmount, serviceLevel) => {
+    if (serviceLevel == "fair"){
         return billAmount * 1.15;
     }   
     if (serviceLevel == "good"){
@@ -24,4 +39,18 @@ const tipAmount = (billAmount, serviceLevel) => {
     }
 }
 
-console.log(tipAmount(100, "good"))
+console.log(totalAmount(50,"fair"))
+
+//Another same as above, but also splits the bill evenly among party members
+const splitBill = (billAmount, serviceLevel, partySize) => {
+    if (serviceLevel == "fair"){
+        return (billAmount * 1.15) / partySize
+    }   
+    if (serviceLevel == "good"){
+        return (billAmount * 1.2) / partySize
+    }
+    if (serviceLevel == "bad"){
+        return (billAmount * 1.1) / partySize
+    }
+}
+console.log(splitBill(75,"bad",8))
